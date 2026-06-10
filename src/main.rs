8d6e0943039b3a45a56a0123b4d0f5f163e25e24
jCabala <kuba.cabala6@gmail.com>
2026-06-10 11:32:19 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,23 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                // Read the request
-                let mut buffer = [0; 1024];
-                _stream.read(&mut buffer).unwrap();
-                let request_str = String::from_utf8_lossy(&buffer);
-                let req = message::Request::from_str(&request_str).unwrap();
+        std::thread::spawn(|| {
+            match stream {
+                Ok(mut _stream) => {
+                    // Read the request
+                    let mut buffer = [0; 1024];
+                    _stream.read(&mut buffer).unwrap();
+                    let request_str = String::from_utf8_lossy(&buffer);
+                    let req = message::Request::from_str(&request_str).unwrap();
 
-                let res = endpoints::handle(&req);
+                    let res = endpoints::handle(&req);
 
-                _stream.write(res.to_string().as_bytes()).unwrap();
+                    _stream.write(res.to_string().as_bytes()).unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        });
     }
 }
